c76501da80ff827083c3a160d4f91e579d23c24d
Betul35 <pskbetulpiyade@gmail.com>
2022-10-20 23:01:12 +0100

    here is my code heheeee

---
+++ src/test/java/g20_DemoGit/BetulsCheerFullCode.java
@@ -0,0 +1,14 @@
+package g20_DemoGit;
+
+public class BetulsCheerFullCode {
+
+    public static void main(String[] args) {
+
+        String perfectClass = "Group 20 is the best one in EU9";
+
+        System.out.println("The truth is: " + perfectClass);
+
+
+    }
+
+}
